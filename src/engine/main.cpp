b23c8c585a27a12b8544f4e9a0e3a53b288131cd
Eric Erasermice <eameres@users.noreply.github.com>
2021-02-25 11:54:25 -0500

    included simplified shaders and mvp

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,16 +1,17 @@
 /* 
-    all-in-one basic OpenGL example 
+    all-in-one basic OpenGL example to accompany Game Architecture HW 3
     created Feb 2021 Eric Ameres
+    ----------------------------
     using STB for graphics loading
     GLEW for OpenGL api wrangling
-    SDL for general applpication "stuff"
+    SDL for general application "stuff"
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+// set up the defines for SDL and STB so that they know this file is "in charge"
 #define SDL_MAIN_HANDLED
-
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <stb_image.h>
@@ -23,16 +24,34 @@
 static const GLuint WIDTH = 512;
 static const GLuint HEIGHT = 512;
 
+// Hint #2... some simple (and not so simple) shaders
+// note that pre/post matrix multiplication is supported, so...
+
+static const GLchar* basic_vertex_shader_source =
+"#version 400\n"
+"layout(location = 0) in vec2 coord2d;\n"
+"void main() {\n"
+"    gl_Position = vec4(coord2d, 0.0, 1.0);\n"
+"}\n";
+
+static const GLchar* basic_fragment_shader_source =
+"#version 400\n"
+"layout(location = 0) out vec4 myColor;\n"
+"void main() {\n"
+"    myColor = vec4(1.0, 0.0, 0.0, 1.0);\n"     // this version uses a fixed color
+"}\n";
+
 static const GLchar* vertex_shader_source =
 "#version 400\n"
 "layout(location = 0) in vec2 coord2d;\n"
 "layout(location = 1) in vec2 vertexUV;\n"
+"uniform mat4 u_mvp;\n"
 "out vec2 fragmentUV;\n"
 "out vec4 interpColor;\n"
 "void main() {\n"
 "    fragmentUV = vertexUV;\n"
 "    interpColor = vec4(coord2d, 0.0, 1.0);\n"
-"    gl_Position = vec4(coord2d, 0.0, 1.0);\n"
+"    gl_Position = u_mvp * vec4(coord2d, 0.0, 1.0);\n"
 "}\n";
 
 static const GLchar* fragment_shader_source =
@@ -55,7 +74,8 @@ static GLfloat positionCoordinates[] = {
     -0.8, -0.4,
      0.0, -0.4,
      0.8, -0.4,
-     0.4,  0.4
+     0.4,  0.4,
+     0.0, -0.4
 }; 
 
 static GLfloat uv[] = {
@@ -63,14 +83,19 @@ static GLfloat uv[] = {
     -0.4, -0.0,
      0.4, -0.0,
      1.2, -0.0,
-     0.8,  0.8
+     0.8,  0.8,
+     0.4, -0.0
 };
 
 static GLshort indices[] = {
      0,1,2,2,3,4,2,0,4
 };
 
+GLfloat myMvp[4][4] = { {1,0,0,0},{0,1,0,0},{0,0,1,0} ,{0,0,0,1} };
+GLfloat myMvp90[4][4] = { { 0, 1, 0, 0 }, { -1, 0, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
 
+/* load a texture from a file using STB */
+/* it is assumed that the path needs to have the executable's location prepended */
 GLuint texture_from_file(const char* path)
 {
     /* first up, resolve the path according to the executable */
@@ -102,7 +127,7 @@ GLuint texture_from_file(const char* path)
     return handle;
 }
 
-
+/* check the shader logs for errors! */
 void check_shader_errors(GLuint shader, char *name) 
 {  
     GLchar* log = NULL;
@@ -122,11 +147,12 @@ void check_shader_errors(GLuint shader, char *name)
     }
 }
 
+/* set up vertex and fragment shaders, combine into a program */
 GLuint common_get_shader_program(
     const char* vertex_shader_source,
     const char* fragment_shader_source
 ) {
-    GLuint fragment_shader, program, vertex_shader;
+    GLuint program, fragment_shader, vertex_shader;
 
     /* Vertex shader */
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -156,6 +182,7 @@ GLuint common_get_shader_program(
     return program;
 }
 
+/* a simple recursive callback to help "animate" our demo */
 int foo = 0;
 SDL_TimerID my_timer_id;
 
@@ -174,18 +201,24 @@ int main(int argc, const char** argv)
 
     set_root_path(argv[0]);
 
-    GLuint program, vbo[4], vao, eLoc, tHandle[2], tLoc, mLoc;
+    GLuint program, basicProgram;   // shader program handles
+    GLuint tHandle[2];              // texture handles
+
+    GLuint vbo[4], vao;             // Vertex Array and Vertex Buffer Object handles
+    
+    GLuint eLoc, tLoc, mLoc, mvpLoc; // uniform variable locations (from shader program)
+    
     SDL_Event event;
     SDL_GLContext gl_context;
     SDL_Window* window;
 
-    GLfloat colorVecBlue[] = { 0.0,0.0,1.0,1.0 };
-    GLfloat colorVecRed[] = { 1.0,0.0,0.0,1.0 };
+    GLfloat colorVecBlue[] = { 0.0,0.0,1.0,1.0 };   // blue
+    GLfloat colorVecRed[] = { 1.0,0.0,0.0,1.0 };    // red
 
     // first we need to set up SLD and glew 
     SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
 
-    my_timer_id = SDL_AddTimer(1000, my_callbackfunc, 0);
+    my_timer_id = SDL_AddTimer(1000, my_callbackfunc, 0); // use an SDL 1 second time to "animate" modes
 
     window = SDL_CreateWindow(__FILE__, 0, 0,
         WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
@@ -196,6 +229,9 @@ int main(int argc, const char** argv)
 
     /* Shader setup. create a program from a vertex and fragment shader combo */
     program = common_get_shader_program(vertex_shader_source, fragment_shader_source);
+    basicProgram = common_get_shader_program(basic_vertex_shader_source, basic_fragment_shader_source);
+
+    // Hint #1...
 
     /* Vertex Array Object setup. */
     glGenVertexArrays(1, &vao);
@@ -218,25 +254,31 @@ int main(int argc, const char** argv)
 
     // third buffer, indices for the vertices used in the geometry
     // notice that we are creating 3 triangles with only 5 vertices ! (some triangles share vertices)
+    // (this means we are likely using glDrawElements, but using glDrawArrays will actually still work, kindof)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[2]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // tell OpenGL to use our shader program
-    glUseProgram(program); 
+    // tell OpenGL which shader program to use
+    glUseProgram(program);
 
     /* Global draw state */
     glViewport(0, 0, WIDTH, HEIGHT);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     
     // the "uniform variables" are used to send data shared by ALL vertices to the shaders
+    // Hint #3...
 
     /* send the color as a uniform vec4 */
     eLoc = glGetUniformLocation(program, "eColor");
     glUniform4fv(eLoc, 1, colorVecBlue); 
     
-    // which texture unit to use (different from which texture to use)
+    /* send the mvp matrix as a uniform Matrix4, note we are NOT transposing the matrix */
+    mvpLoc = glGetUniformLocation(program, "u_mvp");
+    glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, (GLfloat *)myMvp);
+
+    // which texture UNIT to use (different from which texture to use)
     tLoc = glGetUniformLocation(program, "u_texture");
-    glUniform1i(tLoc, 0); // 0: GL_TEXTURE0
+    glUniform1i(tLoc, 0); // 0: GL_TEXTURE0 <- texture unit #0, GPU has at least one!
 
     // select between our custom fragment shader modes
     mLoc = glGetUniformLocation(program, "mode");
@@ -248,7 +290,6 @@ int main(int argc, const char** argv)
     tHandle[1] = texture_from_file("data/textures/brillo.png");
 
     // set the  fill modes for polygons
-
     glPolygonMode(GL_FRONT, GL_FILL);
     glPolygonMode(GL_BACK, GL_FILL);
 
@@ -257,7 +298,15 @@ int main(int argc, const char** argv)
 
         glClear(GL_COLOR_BUFFER_BIT); // clear the background on each iteration
 
-        //  I set up a few "modes" to switch between showing off different renders based on the same vao and vbos
+        // for another quick demo variation, send shader an alternating transformation matrix!
+        if (foo & 1) {
+            glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, (GLfloat*)myMvp);
+        }else{
+            glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, (GLfloat*)myMvp90);
+        }
+
+        //  I set up a few "modes" to switch between showing off different renders 
+        //  based on the same vao and vbos with some tweaks to shader uniforms and glPolygonMode
         switch (foo & 3) {
             case 0:
                 glUniform1i(mLoc, 3); // mode 3 : use texture
@@ -299,9 +348,11 @@ int main(int argc, const char** argv)
                 break;
         }
 
+        // Hint #4...
         /* here's where we actually trigger the rendering, yup, just these few lines! */
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, (void*)0);
+        //glDrawArrays(GL_TRIANGLES, 0, 6 );
 
         /* SDL needs to do a window buffer swap */
         SDL_GL_SwapWindow(window);
@@ -329,6 +380,8 @@ int main(int argc, const char** argv)
     return EXIT_SUCCESS;
 }
 
+
+/* grabs the root path for prepending to data file paths... */
 char g_root_path[256];
 static void set_root_path(const char* exepath)
 {
